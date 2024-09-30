837789687ab1cf849496127ff8d7a060752b70d1
SavaVg <sava.maksimovic@valjevskagimnazija.edu.rs>
2024-09-30 16:23:13 +0200

    Update 11 jmp if else (if else primer collatz-ova transformacija).asm
    pisalo golobal u meto global
    kod uslovnog skoka bile dve tacke na kraju label-a
    dodao komentar

--- 11 jmp if else (if else primer collatz-ova transformacija).asm
+++ 11 jmp if else (if else primer collatz-ova transformacija).asm
@@ -2,7 +2,7 @@
 section .data
 a:resd 1
 section .text
-golobal main
+global main
 main:
 	GET_UDEC 4,[a]
 	;deljenik edx:eax
@@ -12,7 +12,8 @@ main:
 	div ebx
 	cmp edx,0
 	jne skok1
-	jmp stampaj:
+        ; u ovom slucaju ne treba nista vise da radimo jer je izlaz spreman i nalazi se u eax
+	jmp stampaj
 skok1: mov ebx,3
 	mov eax,[a]
 	mul ebx
